f82e6499b50cbdad7c93ca5fdd324766fe55440c
Salman Afridi <salmanafridi.dev@gmail.com>
2026-07-20 12:48:20 +0500

    Utils code discussions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { log } from "console";
+import express from "express";
+import { errorMonitor } from "events";
 
 dotenv.config();
-connectDB();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log("\nServer is listening on port ", port);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoBD connection failed : ", error);
+  });
