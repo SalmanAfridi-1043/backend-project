7d815c171a56337ff54ca8d4fd911fc75ab04b42
Salman Afridi <salmanafridi.dev@gmail.com>
2026-07-24 11:25:53 +0500

    Add Subscription model

---
+++ src/models/Subscription.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema(
+  {
+    subscriber: {
+      type: mongoose.Schema.Types.ObjectId, // subscribing user
+      ref: "User",
+    },
+    channel: {
+      type: mongoose.Schema.Types.ObjectId, // subscriber user
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
